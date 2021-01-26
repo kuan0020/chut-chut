2c536e57e83fb2207aba299a4cb3e08caff1e6c8
kuan0020 <kuan0020@umn.edu>
2021-01-26 13:49:29 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, Response, jsonify
+from bs import get_info
+
+app = Flask(__name__)
+
+items = []
+
+bus_stops = [16086, 16085, 877, 16183, 16111]
+
+for n in bus_stops:
+    items.append(get_info(n))
+
+@app.route('/update_dep', methods=['POST'])
+def updatedep():
+    items = []
+    for n in bus_stops:
+        items.append(get_info(n))
+    return jsonify('', render_template('dep_info_model.html', items=items))
+
+@app.route('/')
+def main():
+    return render_template('index.html', items=items)
+
+if __name__ == "__main__":
+    app.run(host='10.0.0.107')
---
+++ bs.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests 
+from datetime import datetime 
+
+def json_datetime_convert(json_dt):
+    json_dt = str(json_dt)
+    date, time = json_dt.split('T')
+    time = time.rsplit(':',1) # remove seconds from time
+    return str(time[0])
+
+def get_info(bus_stop: str):
+    actual = []
+    dep_text = []
+    # dep_time = []
+    bus_num = []
+    
+    page = requests.get('https://svc.metrotransit.org/NexTrip/' + str(bus_stop))
+    soup = BeautifulSoup(page.content, 'html.parser')
+    nex_trips = soup.find_all('nextripdeparture')
+
+    for i in range(len(nex_trips)):
+        actual.append(nex_trips[i].actual.string)
+        dep_text.append(nex_trips[i].departuretext.string)
+        # dep_time.append(json_datetime_convert(nex_trips[i].departuretime.string))
+        bus_num.append(nex_trips[i].find_all('route')[0].string)
+
+    actual = actual[:len(actual)//4]
+    dep_text = dep_text[:len(dep_text)//4]
+    # dep_time = dep_time[:len(dep_time)//4]
+    bus_num = bus_num[:len(bus_num)//4]
+    
+    items = []
+    for i in range(len(actual)):
+        item = dict(bus_stop=str(bus_stop), actual=actual[i], dep_text=dep_text[i], bus_num=bus_num[i])
+        items.append(item)
+
+    if len(items) == 0:
+        items.append(dict(bus_stop=str(bus_stop), actual='-', dep_text='-', bus_num='-'))
+    return items
