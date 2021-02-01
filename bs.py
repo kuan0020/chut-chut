2924c01e269ce63259fad257026959d10ec7b697
kuan0020 <kuan0020@umn.edu>
2021-02-01 11:48:34 -0600

    new UI and shifted time for accuracy

--- bs.py
+++ bs.py
@@ -9,6 +9,26 @@ def json_datetime_convert(json_dt):
     time = time.rsplit(':',1) # remove seconds from time
     return str(time[0])
 
+def shift_min(info):
+    '''
+    shifts real time info by offset minutes ahead so we won't miss bus lol
+    '''
+    offset = 4
+    
+    if any(char.isdigit() for char in info.split()): # True if info is in the format of '(int) Min', False if info is 'Due' or '(int):(int)'
+        for word in info.split():
+            if word.isdigit():
+                shifted_time = int(word) - offset
+                
+                if shifted_time <= 0: # if shifted time is 0 or negative
+                    txt = 'Due' 
+                else:
+                    txt = info.replace(word, str(shifted_time)) # Replace the (int) with (int-offset)
+    else:
+        txt = info # Does nothing if info is 'Due' or '(int):(int)'
+        
+    return txt
+    
 def get_info(bus_stop: str):
     actual = []
     dep_text = []
@@ -32,9 +52,13 @@ def get_info(bus_stop: str):
     
     items = []
     for i in range(len(actual)):
-        item = dict(bus_stop=str(bus_stop), actual=actual[i], dep_text=dep_text[i], bus_num=bus_num[i])
+        item = dict(bus_stop=str(bus_stop), actual=actual[i], dep_text=shift_min(dep_text[i]), bus_num=bus_num[i])
         items.append(item)
 
     if len(items) == 0:
         items.append(dict(bus_stop=str(bus_stop), actual='-', dep_text='-', bus_num='-'))
     return items
+
+if __name__ == "__main__":
+    info = get_info(16086)
+    print(info)
